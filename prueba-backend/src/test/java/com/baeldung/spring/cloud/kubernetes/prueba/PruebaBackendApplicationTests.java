b962684a734cb9dfb8ef395b6e1a7ef180519f92
charles <chalme220@gmail.com>
2020-03-30 00:29:37 +0200

    demo

---
+++ prueba-backend/src/test/java/com/baeldung/spring/cloud/kubernetes/prueba/PruebaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.spring.cloud.kubernetes.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
